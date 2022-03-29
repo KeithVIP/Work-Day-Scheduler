b5dbe0a4987fca88e2a10027902e863c0b5f6a76
Keith <keithvip@gmail.com>
2022-03-29 16:05:54 -0500

    fina

--- script.js
+++ script.js
@@ -1,7 +1,44 @@
+
 var currentDay = moment().format("dddd, MMMM Do");
-console.log(currentDay);
 
 var element = document.getElementById("currentDay");
 element.append(currentDay);
 console.log
 
+$(this).parent().attr("id")
+$(this).siblings(".description").val()
+
+$(".saveBtn").on("click", function() {
+    localStorage.setItem($(this).parent().attr("id"), $(this).siblings(".description").val());
+})
+
+$("#9 .description").val(localStorage.getItem("9"))
+$("#10 .description").val(localStorage.getItem("10"))
+$("#11 .description").val(localStorage.getItem("11"))
+$("#12.description").val(localStorage.getItem("12"))
+$("#13 .description").val(localStorage.getItem("13"))
+$("#14 .description").val(localStorage.getItem("14"))
+$("#15 .description").val(localStorage.getItem("15"))
+$("#16 .description").val(localStorage.getItem("16"))
+$("#17 .description").val(localStorage.getItem("17"))
+
+
+$(".time-block").each(function() {
+    var currentHour = moment().hours();
+
+    var blockHour = parseInt($(this).attr("id"));
+
+    if (blockHour < currentHour) {
+        $(this).addClass("past");
+
+    } else if(blockHour === currentHour) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+
+    }  else {
+        $(this).removeClass("past present");
+        $(this).addClass("future");
+    }
+})
+    
+    
